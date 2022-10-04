b6c303efc5b0d35cf9023912c60fc9b077934ab8
lcano@unizar.es <lcano@unizar.es>
2022-10-04 18:51:36 +0200

    first test script

---
+++ scripts/test.py
@@ -0,0 +1,13 @@
+# IMPORTS
+import bpy
+# GLOBAL VARIABLES
+SUBT_MODELS_DIRECTORY = "/home/lorenzo/"
+# FUNCTIONS
+
+# CLASSES
+
+# MAIN
+def main():
+    pass
+if __name__ == "__main__":
+    main()
